42d8d05ed2f7f0c44a40ec0979101ac650e88c9f
Annie Berjou <annie.carrau@wanadoo.fr>
2026-05-27 16:25:28 +0200

    ajustement texte description

--- src/components/Description/Description.jsx
+++ src/components/Description/Description.jsx
@@ -1,34 +1,44 @@
 import { NavLink } from "react-router-dom";
+import "./Description.css";
 
 function Description() {
   return (
-    <div className="description">
-      <h1>Accueil</h1>
-      <p>Je crée des interfaces web qui ont du caractère.</p>
-      <p>
-        Développeuse web fraîchement diplômée, passionnée par le front-end, le
-        design soignée et le code propre. Voici un aperçu de mes projets de
-        formation.
-      </p>
-      <NavLink
-        to="/projects"
-        end
-        className={({ isActive }) =>
-          isActive ? "nav-link active" : "nav-link"
-        }
-      >
-        Voir mes projets
-      </NavLink>
-      <NavLink
-        to="/contact"
-        end
-        className={({ isActive }) =>
-          isActive ? "nav-link active" : "nav-link"
-        }
-      >
-        Me contacter
-      </NavLink>
-    </div>
+    <section className="home-description">
+      <div className="description">
+        <h2>
+          Je crée des
+          <span>
+            {" "}
+            interfaces <br /> web{" "}
+          </span>
+          qui ont du <br />
+          caractère.
+        </h2>
+        <p>
+          Développeuse web fraîchement diplômée, passionnée par le front-end, le
+          design soignée et le code propre. Voici un aperçu de mes projets de
+          formation.
+        </p>
+        <NavLink
+          to="/projects"
+          end
+          className={({ isActive }) =>
+            isActive ? "nav-link active" : "nav-link"
+          }
+        >
+          Voir mes projets
+        </NavLink>
+        <NavLink
+          to="/contact"
+          end
+          className={({ isActive }) =>
+            isActive ? "nav-link active" : "nav-link"
+          }
+        >
+          Me contacter
+        </NavLink>
+      </div>
+    </section>
   );
 }
 
