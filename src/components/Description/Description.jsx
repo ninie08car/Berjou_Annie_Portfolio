ec1a255cba7f2593f7f88944f5440ed81ea8d9fa
Annie Berjou <annie.carrau@wanadoo.fr>
2026-05-31 20:00:17 +0200

    Ajout section A propos et Parcours

--- src/components/Description/Description.jsx
+++ src/components/Description/Description.jsx
@@ -14,27 +14,38 @@ function Description() {
           qui ont du <br />
           caractère.
         </h2>
-        <p>
-          Bonjour, je suis Annie Berjou, développeuse web junior passionnée par
-          le design d’interfaces modernes et le développement avec React. <br />{" "}
-          Après plus de 20 ans comme responsable fichier dans le commerce
-          alimentaire, j’ai choisi de me reconvertir dans le développement web,
-          un domaine qui m’intéressait déjà lors de mes études en BTS
-          Informatique de gestion. Aujourd’hui, je développe mes compétences à
-          travers une formation et des projets concrets. <br />
-          Je maîtrise principalement HTML, CSS et React, et je continue à
-          approfondir JavaScript. J’aime particulièrement créer des interfaces
-          responsive, soignées et agréables à utiliser. <br />
-          Parmi mes projets, Booki représente une étape importante car il s’agit
-          de mon premier projet réalisé après plusieurs années sans pratiquer le
-          développement. Ohmyfood m’a également permis de relever un vrai défi
-          autour des animations CSS, tandis que Kasa m’a aidée à approfondir
-          React. <br />
-          Persévérante et motivée, j’apprécie autant le travail en autonomie
-          qu’en équipe. Je recherche aujourd’hui une première opportunité pour
-          acquérir de l’expérience et continuer à progresser, notamment dans les
-          animations web et le back-end.
-        </p>
+        {/*SECTION A PROPOS */}
+        <div className="about-section">
+          <h3>À propos</h3>
+          <p>
+            Bonjour, je suis Annie Berjou, développeuse web junior passionnée
+            par le design d’interfaces modernes et le développement avec React.
+            Après plus de 20 ans comme responsable fichier dans le commerce
+            alimentaire, j’ai choisi de me reconvertir dans le développement
+            web, un domaine qui m’intéressait déjà lors de mes études en BTS
+            Informatique de gestion. Aujourd’hui, je développe mes compétences à
+            travers une formation et des projets concrets.
+          </p>
+        </div>
+        {/* SECTION PARCOURS */}
+        <div className="journey-section">
+          <h3>Parcours</h3>
+          <p>
+            Je maîtrise principalement HTML, CSS et React, et je continue à
+            approfondir JavaScript. J’aime particulièrement créer des interfaces
+            responsive, soignées et agréables à utiliser. Parmi mes projets,
+            Booki représente une étape importante car il s’agit de mon premier
+            projet réalisé après plusieurs années sans pratiquer le
+            développement. Ohmyfood m’a également permis de relever un vrai défi
+            autour des animations CSS, tandis que Kasa m’a aidée à approfondir
+            React. Persévérante et motivée, j’apprécie autant le travail en
+            autonomie qu’en équipe. Je recherche aujourd’hui une première
+            opportunité pour acquérir de l’expérience et continuer à progresser,
+            notamment dans les animations web et le back-end.
+          </p>
+        </div>
+
+        {/*BOUTONS*/}
         <div className="description-buttons">
           <NavLink to="/projects" className="btn-primary">
             Voir mes projets
