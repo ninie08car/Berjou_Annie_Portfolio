ee12f43d0260d1e46079df95300a7d8ef69b3e60
Annie Berjou <annie.carrau@wanadoo.fr>
2026-06-02 18:31:52 +0200

    Correction du base

--- src/routes/PortfolioRouter.jsx
+++ src/routes/PortfolioRouter.jsx
@@ -9,7 +9,7 @@ import Footer from "../components/Footer/Footer";
 
 function PortfolioRouter() {
   return (
-    <Router basename="/Berjou_Annie_Portfolio">
+    <Router basename="/Berjou_Annie_Portfolio/">
       <Header />
       <Routes>
         {/* Route Accueil */}
